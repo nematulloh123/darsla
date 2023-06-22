b5e9a8fd5be11406a5bc35d4b07fe3c8772306f6
nematullohxolmatov <nematulohx@gmail.com>
2023-06-22 15:28:02 +0500

    first commit

---
+++ start.py
@@ -0,0 +1,57 @@
+#1
+#def multiply_list_elements(list1):
+#    result = 1
+#    for i in list1:
+#        result *= i
+#    return result
+
+#list = [8,3,4,2,1]
+#print(multiple_list(list1))
+
+#2
+#def reverse_string(string):
+#    return string[::-1]
+
+#matn1 = "Everyone"
+#print(reverse_string(matn1))
+
+#3
+
+#def print_squares():
+#    for i in range(1, 21):
+#        print(i**2)
+#        print_squares()
+
+
+#4
+#def check_even_odd(num):
+#    if num % 2 == 0:
+#        print(num, "juft son")
+#    else:
+#        print(num, "toq son")
+#        num = int(input("Iltimos, son kiriting: "))
+#    check_even_odd(num)
+
+#5
+#def uppercase(string):
+#    return string.upper()
+#def lowercase(string):
+#    return string.lower()
+#test_string = "Bu Katta Harflar va Kichik Harflar"
+#print(uppercase(test_string))
+#print(lowercase(test_string))
+
+#6
+#def calculate_circle_area(radius):
+#    pi = 3.14159
+#    return pi * (radius**2)
+#radius = float(input("Doira radiusini kiriting: "))
+#area = calculate_circle_area(radius)
+#print("Doira yuzasi:", area)
+
+#7
+#def remove_a(string):
+#    return string.replace("a", "")
+#str1 = "Assalomu aleykum xammaga"
+#result = remove_a(str1)
+#print(result)
